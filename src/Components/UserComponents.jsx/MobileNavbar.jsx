1ff23c79db21581974d8df2b4570f34d825b8354
Adhars <138186421+Adharsraj@users.noreply.github.com>
2023-09-05 16:38:14 +0530

    fix/mobilenav

--- src/Components/UserComponents.jsx/MobileNavbar.jsx
+++ src/Components/UserComponents.jsx/MobileNavbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Navbar,
   MobileNav,
@@ -7,39 +7,186 @@ import {
   IconButton,
 } from "@material-tailwind/react";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
  
 export function NavbarDefault() {
   const [openNav, setOpenNav] = React.useState(false);
- 
+ const[name,setName]=useState("")
+
+
+useEffect(()=>{
+const ls=JSON.parse(localStorage.getItem("User"))
+console.log(ls)
+setName(ls)
+},[])
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false),
     );
   }, []);
- 
+        
   const navList = (
+
+
+
+  //   <div className=" text-">
+  //   <List>
+  //     <Link to="/admin/home">
+  //       <ListItem className="text-white flex border mt-2   text-lg">
+  //         <ListItemPrefix>
+  //           <HomeIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Dashboard
+  //       </ListItem>
+  //     </Link>
+
+  //     <Accordion
+  //       open={open === 2}
+  //       icon={
+  //         <ChevronDownIcon
+  //           strokeWidth={2.5}
+  //           className={`mx-auto h-4 w-4 text-white transition-transform ${
+  //             open === 2 ? "rotate-180" : ""
+  //           }`}
+  //         />
+  //       }
+  //     >
+  //       <ListItem className="p-0 border mt-2" selected={open === 2}>
+  //         <AccordionHeader
+  //           onClick={() => handleOpen(2)}
+  //           className="border-b-0 text-white p-3"
+  //         >
+  //           <ListItemPrefix>
+  //             <TableCellsIcon className="h-6 w-6" />
+  //           </ListItemPrefix>
+  //           <Typography
+  //             color="blue-gray"
+  //             className="mr-auto text-white text-lg font-semibold"
+  //           >
+  //             Tables
+  //           </Typography>
+  //         </AccordionHeader>
+  //       </ListItem>
+  //       <AccordionBody className="py-1 text-white">
+  //         <List className="p-0">
+  //           <Link to="/admin/userlist">
+  //             <ListItem className="text-white">
+  //               <ListItemPrefix>
+  //                 <StarIcon strokeWidth={3} className="h-3 text w-5" />
+  //               </ListItemPrefix>
+  //               User Tables
+  //             </ListItem>
+  //           </Link>
+  //           <Link to="/admin/electionlist">
+  //             <ListItem className="text-white">
+  //               <ListItemPrefix className="text-white">
+  //                 <StarIcon strokeWidth={3} className="h-3 w-5 " />
+  //               </ListItemPrefix>
+  //               Election Tables
+  //             </ListItem>
+  //           </Link>
+  //         </List>
+  //       </AccordionBody>
+  //     </Accordion>
+
+  //     <Link to="/admin/results">
+  //       <ListItem className="text-white border mt-2 flex   text-lg">
+  //         <ListItemPrefix>
+  //           <FlagIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Results
+  //       </ListItem>
+  //     </Link>
+  //     <Link to="/admin/notvoted">
+  //       <ListItem className="text-white flex border mt-2  text-lg">
+  //         <ListItemPrefix>
+  //           <XCircleIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Not Voted List
+  //       </ListItem>
+  //     </Link>
+
+  //     <Link to="/admin/createuser">
+  //       <ListItem className="text-white border mt-2 flex   text-lg">
+  //         <ListItemPrefix>
+  //           <UserIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Create User
+  //       </ListItem>
+  //     </Link>
+  //     <Link to="/admin/createelection">
+  //       <ListItem className="text-white border mt-2 flex  text-lg">
+  //         <ListItemPrefix>
+  //           <ClipboardDocumentCheckIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Create Election
+  //       </ListItem>
+  //     </Link>
+  //     {/* <Link to="/admin/createcandidate">
+  //       <ListItem className="text-white border mt-2 flex   text-lg">
+  //         <ListItemPrefix>
+  //           <UserPlusIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Create Candidate
+  //       </ListItem>
+  //     </Link> */}
+  //     <Link to="/admin/offlinevote">
+  //       <ListItem className="text-white border mt-2 flex   text-lg">
+  //         <ListItemPrefix>
+  //           <ExclamationTriangleIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Offline Vote
+  //       </ListItem>
+  //     </Link>
+
+  //     <Link to="/">
+  //       <ListItem className="text-white border mt-2 flex   text-lg">
+  //         <ListItemPrefix>
+  //           <PowerIcon className="h-6 w-6 " />
+  //         </ListItemPrefix>
+  //         Log out
+  //       </ListItem>
+  //     </Link>
+  //   </List>
+  // </div>
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className="p-1 text-lg "
       >
-        <a href="#" className="flex items-center">
-          View Results
-        </a>
+       <Link to="/home">
+        Home
+
+       </Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className="p-1 text-lg "
       >
-        <a href="#" className="flex items-center">
-          Logout
-        </a>
+       <Link to="/result">
+          Results
+
+       </Link>
       </Typography>
+     
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 text-lg "
+      >
+       <Link to="/">
+          Logout
+
+       </Link>
+      </Typography>
+     
    
     </ul>
   );
@@ -52,17 +199,19 @@ export function NavbarDefault() {
           href="#"
           className="mr-4 cursor-pointer py-1.5 font-medium"
         >
-          Material Tailwind
+          Election
         </Typography>
       <div className="flex items-center gap-1">
-        <h1>Tom Cruise</h1>
-        <UserCircleIcon
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
+        <div     className="flex items-center gap-2"        onClick={() => setOpenNav(!openNav)}
+>
+        <h1>{name&&name.username}</h1>
+        <img
+        src={name&&name.url}
+        className="w-10 h-10 rounded-full "
+        />
+
+        </div>
+          {/* {openNav ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -91,8 +240,7 @@ export function NavbarDefault() {
                 d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
-          )}
-        </UserCircleIcon>
+          )} */}
         </div>
 
       </div>
@@ -108,36 +256,225 @@ export function NavbarDefault() {
 
 export function NavbarDefaultAdmin() {
     const [openNav, setOpenNav] = React.useState(false);
-   
+   const[name,setName]=useState("")
+
+
+useEffect(()=>{
+  const ls=JSON.parse(localStorage.getItem("User"))
+  console.log(ls)
+  setName(ls)
+},[])
+
     React.useEffect(() => {
       window.addEventListener(
         "resize",
         () => window.innerWidth >= 960 && setOpenNav(false),
       );
     }, []);
-   
+          
     const navList = (
+
+
+
+    //   <div className=" text-">
+    //   <List>
+    //     <Link to="/admin/home">
+    //       <ListItem className="text-white flex border mt-2   text-lg">
+    //         <ListItemPrefix>
+    //           <HomeIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Dashboard
+    //       </ListItem>
+    //     </Link>
+
+    //     <Accordion
+    //       open={open === 2}
+    //       icon={
+    //         <ChevronDownIcon
+    //           strokeWidth={2.5}
+    //           className={`mx-auto h-4 w-4 text-white transition-transform ${
+    //             open === 2 ? "rotate-180" : ""
+    //           }`}
+    //         />
+    //       }
+    //     >
+    //       <ListItem className="p-0 border mt-2" selected={open === 2}>
+    //         <AccordionHeader
+    //           onClick={() => handleOpen(2)}
+    //           className="border-b-0 text-white p-3"
+    //         >
+    //           <ListItemPrefix>
+    //             <TableCellsIcon className="h-6 w-6" />
+    //           </ListItemPrefix>
+    //           <Typography
+    //             color="blue-gray"
+    //             className="mr-auto text-white text-lg font-semibold"
+    //           >
+    //             Tables
+    //           </Typography>
+    //         </AccordionHeader>
+    //       </ListItem>
+    //       <AccordionBody className="py-1 text-white">
+    //         <List className="p-0">
+    //           <Link to="/admin/userlist">
+    //             <ListItem className="text-white">
+    //               <ListItemPrefix>
+    //                 <StarIcon strokeWidth={3} className="h-3 text w-5" />
+    //               </ListItemPrefix>
+    //               User Tables
+    //             </ListItem>
+    //           </Link>
+    //           <Link to="/admin/electionlist">
+    //             <ListItem className="text-white">
+    //               <ListItemPrefix className="text-white">
+    //                 <StarIcon strokeWidth={3} className="h-3 w-5 " />
+    //               </ListItemPrefix>
+    //               Election Tables
+    //             </ListItem>
+    //           </Link>
+    //         </List>
+    //       </AccordionBody>
+    //     </Accordion>
+
+    //     <Link to="/admin/results">
+    //       <ListItem className="text-white border mt-2 flex   text-lg">
+    //         <ListItemPrefix>
+    //           <FlagIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Results
+    //       </ListItem>
+    //     </Link>
+    //     <Link to="/admin/notvoted">
+    //       <ListItem className="text-white flex border mt-2  text-lg">
+    //         <ListItemPrefix>
+    //           <XCircleIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Not Voted List
+    //       </ListItem>
+    //     </Link>
+
+    //     <Link to="/admin/createuser">
+    //       <ListItem className="text-white border mt-2 flex   text-lg">
+    //         <ListItemPrefix>
+    //           <UserIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Create User
+    //       </ListItem>
+    //     </Link>
+    //     <Link to="/admin/createelection">
+    //       <ListItem className="text-white border mt-2 flex  text-lg">
+    //         <ListItemPrefix>
+    //           <ClipboardDocumentCheckIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Create Election
+    //       </ListItem>
+    //     </Link>
+    //     {/* <Link to="/admin/createcandidate">
+    //       <ListItem className="text-white border mt-2 flex   text-lg">
+    //         <ListItemPrefix>
+    //           <UserPlusIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Create Candidate
+    //       </ListItem>
+    //     </Link> */}
+    //     <Link to="/admin/offlinevote">
+    //       <ListItem className="text-white border mt-2 flex   text-lg">
+    //         <ListItemPrefix>
+    //           <ExclamationTriangleIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Offline Vote
+    //       </ListItem>
+    //     </Link>
+
+    //     <Link to="/">
+    //       <ListItem className="text-white border mt-2 flex   text-lg">
+    //         <ListItemPrefix>
+    //           <PowerIcon className="h-6 w-6 " />
+    //         </ListItemPrefix>
+    //         Log out
+    //       </ListItem>
+    //     </Link>
+    //   </List>
+    // </div>
       <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
         <Typography
           as="li"
           variant="small"
           color="blue-gray"
-          className="p-1 font-normal"
+          className="p-1 text-lg "
         >
-          <a href="#" className="flex items-center">
-            View Results
-          </a>
+         <Link to="/admin/home">
+          Home
+
+         </Link>
         </Typography>
         <Typography
           as="li"
           variant="small"
           color="blue-gray"
-          className="p-1 font-normal"
+          className="p-1 text-lg "
         >
-          <a href="#" className="flex items-center">
-            Logout
-          </a>
+         <Link to="/admin/results">
+            Results
+
+         </Link>
         </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 text-lg "
+        >
+      <Link to="/admin/notvoted">
+            Not voted
+
+         </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 text-lg "
+        >
+      <Link to="/admin/createuser">
+            Create user
+
+         </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 text-lg "
+        >
+         <Link to="/admin/createelection">
+            Create Election
+
+         </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 text-lg "
+        >
+         <Link to="/admin/offlinevote">
+            Offline vote
+
+         </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 text-lg "
+        >
+         <Link to="/">
+            Logout
+
+         </Link>
+        </Typography>
+       
      
       </ul>
     );
@@ -150,17 +487,19 @@ export function NavbarDefaultAdmin() {
             href="#"
             className="mr-4 cursor-pointer py-1.5 font-medium"
           >
-            Material Tailwind
+            Election
           </Typography>
         <div className="flex items-center gap-1">
-          <h1>Tom Cruise</h1>
-          <UserCircleIcon
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
+          <div     className="flex items-center gap-2"        onClick={() => setOpenNav(!openNav)}
+ >
+          <h1>{name&&name.username}</h1>
+          <img
+          src={name&&name.url}
+          className="w-10 h-10 rounded-full "
+          />
+
+          </div>
+            {/* {openNav ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -189,8 +528,7 @@ export function NavbarDefaultAdmin() {
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
-            )}
-          </UserCircleIcon>
+            )} */}
           </div>
   
         </div>
